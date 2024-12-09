1c40559d554064c40f8d395be7a54a0db84b29de
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2024-12-09 19:26:13 +0400

    Completed code

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ function showToast(msg){
     toast.classList.add('toast');
     toast.innerHTML = msg;
     toastBox.appendChild(toast);
+
+    if(msg.includes('error')){
+        toast.classList.add('error');
+    }
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid');
+    }
+
+    setTimeout(()=>{
+        toast.remove();
+    },3000);
 }
